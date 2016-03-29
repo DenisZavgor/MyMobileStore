9fc42e21dc348d8a1172e0ba3c2639c0f4e5de10
web <web@training.workrocks.ua>
2016-03-29 20:59:42 +0300

    fist

---
+++ client/src/app/index.route.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('client')
+    .config(routerConfig);
+
+  /** @ngInject */
+  function routerConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('details', {
+        url: '/details',
+        templateUrl: 'app/details/details.view.html',
+        controller: 'DetailsController',
+        controllerAs: 'details'
+      });
+
+      
+    $urlRouterProvider.otherwise('/');
+  }
+
+})();
